00b04c6f9ae1779795b7ac3227bf2889e9c3c199
VictorLpzV <100876529+VictorLpzV@users.noreply.github.com>
2022-10-18 22:14:10 -0500

    Add files via upload
    Actualización de ejercicios de angular

---
+++ calculadora-app/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Aplicación calculadora';
+  resultadoPadre:number;
+
+ procesarResultado(resultado:number){
+  this.resultadoPadre=resultado;
+ }
+}
